6398900ae3c0ab8a6f98aff8688468fae316a2f6
Ryan Nelson <ryannelson6@hotmail.com>
2025-06-15 14:07:18 -0700

    Simple comments on failables

--- utils.h
+++ utils.h
@@ -12,6 +12,7 @@ static inline void ip_to_string(struct sockaddr_in* ip, char buf[IPV4_MAX_LENGTH
 	strcpy(buf, inet_ntoa(ip->sin_addr));
 }
 
+// returns true on failure, else false
 static inline bool string_to_ip(struct sockaddr_in* ip, char buf[IPV4_MAX_LENGTH]) {
 	return !inet_aton(buf, &ip->sin_addr);
 }
@@ -20,6 +21,7 @@ static inline void port_to_string(struct sockaddr_in* ip, char buf[PORT_MAX_LENG
 	sprintf(buf, "%hu", &ip->sin_port);
 }
 
+// returns true on failure, else false
 static inline bool string_to_port(struct sockaddr_in* ip, char buf[PORT_MAX_LENGTH]) {
 	return !sscanf(buf, "%hu", &ip->sin_port);
 }
